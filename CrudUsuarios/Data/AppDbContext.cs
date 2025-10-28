166e426efc42fc3ed9e08b8c80e03998f3e628c5
JulioBendz <bengutjul2@gmail.com>
2025-10-28 03:45:23 -0500

    Add AppDbContext class for Entity Framework Core integration

---
+++ CrudUsuarios/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿// Entity Framework Core version: 7.0.0(2 files)
+using Microsoft.EntityFrameworkCore;
+using CrudUsuarios.Models;
+
+namespace CrudUsuarios.Data
+{
+    public class AppDbContext : DbContext
+    {
+        // 👇 Constructor que recibe las opciones de configuración (4 files)
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // 👇 Aquí defines las tablas que quieres crear en la BD (1 file)
+        public DbSet<Usuarios> Usuarios { get; set; }
+    }
+}
